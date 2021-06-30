ceaaa83b786aa1d41f671a5d48c55ce0f28f5f16
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2021-06-30 18:13:57 +0200

    fix: use string template

--- src/eslint-json-report-to-js.ts
+++ src/eslint-json-report-to-js.ts
@@ -9,7 +9,7 @@ export default function ESLintJsonReportToJS(): ESLintReport {
   const report = core.getInput('report-json', { required: false }) || 'eslint_report.json';
   const reportPath = path.resolve(report);
   if (!fs.existsSync(reportPath)) {
-    core.setFailed('The report-json file "${report}" could not be resolved.');
+    core.setFailed(`The report-json file "${report}" could not be resolved.`);
   }
   const reportContents = fs.readFileSync(reportPath, 'utf-8');
   return JSON.parse(reportContents);
